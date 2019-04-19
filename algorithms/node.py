7ca0079211ce7b37cfd1630f17ff6bd708662d6e
Michaela Buchanan <buchanam@oregonstate.edu>
2019-04-18 21:25:48 -0700

    added astar which seems to work

--- algorithms/node.py
+++ algorithms/node.py
@@ -7,6 +7,7 @@ class Node:
     def __init__(self, data, parent, children):
         self.__data = data
         self.__parent = parent
+        self.__f_val = None
         if children != None:
             for child in children:
                 self.children.append(child)
@@ -21,3 +22,21 @@ class Node:
     @property
     def parent(self):
         return self.__parent
+
+    @property
+    def f_val(self):
+        return self.__f_val
+
+    def generate_fval(self, goal):
+        x = len(self.__data)
+        total = 0
+        # heuristic total
+        for y in range(0, x):
+            total += abs(self.__data[y] - goal[y])
+        # cost total
+
+        parent_node = self.__parent
+        while (parent_node != None):
+            total += 1
+            parent_node = parent_node.parent
+        self.__f_val = total
